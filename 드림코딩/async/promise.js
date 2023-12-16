678820ef6430c1c03f1100bfdf12c276bd54e80f
suhyug <geekplay@naver.com>
2023-12-16 14:45:37 +0900

    1216_dreamCoding

---
+++ 드림코딩/async/promise.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//promise is a JavaScript object for asynchronous operation.
+//state: pending => fulfilled or rejected
+//producer VS consumer
+
+//1.producer
+//when new promise is created, the excutor runs automatically.
+const promise = new Promise((resole,reject)=>{
+
+})
